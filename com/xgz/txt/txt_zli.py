494da40cfcc091fa05158601ae1f2b3d1dafefb4
XGZ <XGZ@com>
2022-09-30 22:58:38 +0800

    修补export \w+="11407766_11662916"匹配_没有匹配到_问题

--- com/xgz/txt/txt_zli.py
+++ com/xgz/txt/txt_zli.py
@@ -60,8 +60,8 @@ def tx_revise():
                             # 跳过本次循环
                             continue
                     ex_ht = re.findall('.*?(export [0-9a-zA-Z_]+="(?:<a href=")?https://.*?")', j, re.S)
-                    ex_tx = re.findall(r'.*?(export [0-9a-zA-Z_]+="?[A-Za-z0-9&]+"?)', j, re.S)
-                    ht_tx = re.findall(r'(https://.*?)"', j, re.S)
+                    ex_tx = re.findall(r'.*?(export [0-9a-zA-Z_]+="?[A-Za-z0-9&_]+"?)', j, re.S)
+                    ht_tx = re.findall(r'(https://.*_?)"', j, re.S)
                     # 如果开头是export =后面有"https://则添加到文本中
                     if ex_ht:
                         ht = re_exht(file_new, ex_ht, marks)
